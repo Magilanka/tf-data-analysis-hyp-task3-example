d4e877fe301452684d529aa179b4a96983eb3f51
Марина <Magilanka@users.noreply.github.com>
2023-04-10 23:34:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,8 +4,28 @@ import numpy as np
 
 chat_id = 706100023 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(data1: int, 
+        data2: int)-> bool:
+  from scipy.stats import t
+  # Вычисление средних значений и стандартных отклонений
+  mean1 = np.mean(data1)
+  mean2 = np.mean(data2)
+  std1 = np.std(data1, ddof=1)
+  std2 = np.std(data2, ddof=1)
+
+  # Вычисление статистического показателя
+  n1 = len(data1)
+  n2 = len(data2)
+  t_stat = (mean1 - mean2) / np.sqrt((std1**2 / n1) + (std2**2 / n2))
+
+  # Вычисление критического значения
+  alpha = 0.05
+  df = n1 + n2 - 2
+  critical_value = t.ppf(1 - alpha, df)
+
+  # Проверка гипотезы
+  if t_stat > critical_value or t_stat < -critical_value:
+    rez = 1 # да отклонить нулевую гипотезу
+  else:
+    rez = 0 # нет, не отверганем, нулевая остается, не отклонять
+  return bool(rez)
